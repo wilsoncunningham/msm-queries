4a0771513e9a1bb6042b9318cf42b1106ec597da
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-02-12 01:38:16 +0000

    progress with details

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,4 +4,12 @@ class DirectorsController < ApplicationController
     render({:template => "director_templates/list"})
   end
 
+  def show
+    @director_id = params.fetch("director_id")
+    matching_record = Director.all.where({:id => @director_id})
+    @director = matching_record[0]
+
+    render({:template => "director_templates/details"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors", {:controller => "directors", :action => "index"})
+  get("/directors/:director_id", {:controller => "directors", :action => "show"})
 end
