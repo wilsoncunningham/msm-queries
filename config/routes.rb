5b7d63f099114efa93bc58d574a6d7ae8d9b1c6b
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-02-12 01:23:52 +0000

    directors list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
-  get("/directors", {:controller => "director", :action => }
+  get("/directors", {:controller => "directors", :action => "index"})
 end
